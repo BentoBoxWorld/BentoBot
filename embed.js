0ed020c03bd5606a93fd224cf6f9fec1b4b373ca
Florian CUNY <poslovitch@bentobox.world>
2020-08-24 22:41:32 +0200

    Changed icon/text for embeds

--- embed.js
+++ embed.js
@@ -2,8 +2,8 @@ exports.EmbedGen = function (recembed) {
     const sentembed = recembed;
     sentembed.color = 16096256;
     sentembed.footer = {
-        "icon_url": "https://cdn.discordapp.com/avatars/740219687459356792/fb089387bba8587e43092557055d6cc4.png",
-        "text": "BentoBot"
+        "icon_url": "https://cdn.discordapp.com/avatars/744954751598329858/f765dbdaa43f6eb047b9e1bf4eb37f04.png",
+        "text": "BentoBot by BentoBoxWorld"
     };
     sentembed.timestamp = new Date()
     return sentembed;
