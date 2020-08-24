c62f352d283dabe7faa481bd411adea0649ac2a3
Florian CUNY <poslovitch@bentobox.world>
2020-08-24 22:26:05 +0200

    Removed avatar and server command

--- commands/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Display info about this server.',
-	permission: 'EVERYONE',
-	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-};
