dd17c1d07d8a0057ee1fe84565c12bc3db7fc378
Florian CUNY <poslovitch@bentobox.world>
2020-08-24 22:41:16 +0200

    Changed description of the docs command

--- commands/docs.js
+++ commands/docs.js
@@ -1,12 +1,12 @@
 const Embeds = require('../embed.js');
 module.exports = {
 	name: 'docs',
-	description: 'The BentoBox Docs',
+	description: 'provides a link to the documentation',
 	permission: 'EVERYONE',
 	cooldown: 5,
 	execute(message) {
 		const CreatedEmbed = Embeds.EmbedGen({
-				"title": "BentoBoxWorld Documentation",
+				"title": "Documentation",
 				"description": "Our documentation is available at [docs.bentobox.world](https://docs.bentobox.world)."
 		});
 		message.channel.send({ embed: CreatedEmbed })
