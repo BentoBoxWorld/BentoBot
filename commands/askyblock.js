cbed6117737223ae6ab0180cc5460a0cc730a228
Florian CUNY <poslovitch@bentobox.world>
2020-08-25 23:19:23 +0200

    Slightly improved the askyblock description

--- commands/askyblock.js
+++ commands/askyblock.js
@@ -1,13 +1,13 @@
 const Embeds = require('../embed.js');
 module.exports = {
 	name: 'askyblock',
-	description: 'ASkyBlock is unsupported',
+	description: 'displays information about ASkyBlock',
 	permission: 'EVERYONE',
 	cooldown: 5,
 	execute(message) {
 		const CreatedEmbed = Embeds.EmbedGen({
 			"title": "ASkyBlock",
-			"description": "ASkyBlock (1.8 - 1.12.2) Is Now Unsupported, and Archived on github. https://github.com/tastybento/askyblock"
+			"description": "ASkyBlock (1.8 - 1.12.2) is unsupported. Its source code can still be found on [GitHub](https://github.com/tastybento/askyblock)."
 		});
 		message.channel.send({ embed: CreatedEmbed })
 	},
