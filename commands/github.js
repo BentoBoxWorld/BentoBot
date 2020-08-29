507366e198902bbfb71266ddb028279a16a43624
Florian CUNY <poslovitch@bentobox.world>
2020-08-30 00:02:23 +0200

    Improved !github to provide links for specific repos

--- commands/github.js
+++ commands/github.js
@@ -1,15 +1,25 @@
 const Embeds = require('../embed.js');
+const discord = require('discord.js');
+const aliases = require('../aliases.js')
+
 module.exports = {
 	name: 'github',
-	description: 'Bentobox Github',
+	description: 'provides links to a GitHub repository',
 	permission: 'EVERYONE',
+	usage: '<repo>',
 	aliases: ['gh', 'repository', 'repo'],
 	cooldown: 5,
-	execute(message) {
-		const CreatedEmbed = Embeds.EmbedGen({
-			"title": "BentoBox Github",
-			"description": "The Bentobox Github is located at https://bentobox.world"
-		});
-		message.channel.send({ embed: CreatedEmbed })
+	execute(message, args) {
+		if (args.length == 1) {
+      let project = aliases.find_project_for_alias(args[0]);
+			const embed = new discord.MessageEmbed()
+			embed.setTitle(`${project} GitHub repository`);
+			embed.setDescription(`[Click here to go to the ${project} repository](https://github.com/BentoBoxWorld/${project}).`);
+			embed.addFields(
+				{ name: "Additional links", value: "[Issue tracker](https://github.com/BentoBoxWorld/${project}/issues) • [Releases](https://github.com/BentoBoxWorld/${project}/releases) • [Contributors](https://github.com/BentoBoxWorld/${project}/graphs/contributors)" }
+			);
+			const CreatedEmbed = Embeds.EmbedGen(embed);
+		  message.channel.send({ embed: CreatedEmbed })
+		}
 	},
 };
