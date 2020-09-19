6d5947efda2795bc7df4ac43fb923847c34eb909
Florian CUNY <poslovitch@gmail.com>
2020-09-19 22:30:37 +0200

    Fixed typo in javadocs.js

--- commands/javadocs.js
+++ commands/javadocs.js
@@ -7,7 +7,7 @@ module.exports = {
 	execute(message) {
 		const CreatedEmbed = Embeds.EmbedGen({
 				"title": "Javadocs",
-				"description": "Our Javadocs our availa at [ci.codemc.io/job/BentoBoxWorld/job/BentoBox/ws/target/apidocs/index.html](https://ci.codemc.io/job/BentoBoxWorld/job/BentoBox/ws/target/apidocs/index.html). \n **Warning:** You may have to refresh the page once, for it to load"
+				"description": "Our Javadocs our available at [ci.codemc.io/job/BentoBoxWorld/job/BentoBox/ws/target/apidocs/index.html](https://ci.codemc.io/job/BentoBoxWorld/job/BentoBox/ws/target/apidocs/index.html). \n **Warning:** You may have to refresh the page once, for it to load"
 		});
 		message.channel.send({ embed: CreatedEmbed })
 	}
